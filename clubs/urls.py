9d5dc9788184d07a0ca0fc6cc3429abe10e85984
3x-haust <lyusungyun@icloud.com>
2024-08-15 18:51:50 +0900

    Feat: merging backend and frontend, multilingual support

--- clubs/urls.py
+++ clubs/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('clubs/<str:name>/logs/', views.add_log, name='add_log'),
-    path('clubs/<str:name>/', views.get_club_info, name='get_club_info'),
-    path('clubs/<str:name>/info', views.update_club_info, name='update_club_info'),
+    path('<str:name>/logs/', views.add_log, name='add_log'),
+    path('<str:name>/', views.get_club_info, name='get_club_info'),
+    path('<str:name>/info', views.update_club_info, name='update_club_info'),
 ]
--- joinus/urls.py
+++ joinus/urls.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.conf.urls.i18n import i18n_patterns
+
+from joinus.views import home, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('clubs.urls')),
+    path('clubs/', include('clubs.urls')),
+    path('', index),
+    path('home/', home),
 ]
+
+urlpatterns += i18n_patterns(
+    path('', index),
+    path('home/', home),
+)
