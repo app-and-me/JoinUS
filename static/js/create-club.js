b6c70dd6d44288b95d32a889ad5ea5d06f244e5a
juyeon Bae <w2316@e-mirim.hs.kr>
2024-08-20 02:35:13 +0900

    feat : 동아리 생성시 popup창 띄우기

--- static/js/create-club.js
+++ static/js/create-club.js
@@ -4,13 +4,30 @@ document.getElementById('create-club').addEventListener('submit', function(event
     const clubName = document.getElementById('clubName').value;
     const simpleIntroduce = document.getElementById('simpleIntroduce').value;
     const clubIntroduce = document.getElementById('clubIntroduce').value;
-    const clubMemberNumber = document.getElementById('clubMemberNumber');
+    const clubMemberNumber = document.getElementById('clubMemberNumber').value;
     const clubImgUrl = document.getElementById('clubImgUrl').value;
 
-    if(!clubName || !simpleIntroduce || !clubIntroduce || !clubMemberNumber || !clubImgUrl){
-        return;
-    }
+    // 모든 필드가 채워졌는지 확인
+    if(clubName && simpleIntroduce && clubIntroduce && clubMemberNumber && clubImgUrl) {
+        document.querySelector('.club-name div').innerHTML = clubName;
 
-    window.location.href='index.html'
-
+        popup.style.display = 'block';
+        overlay.style.display = 'block';
+    }  
 });
+
+// 팝업 열기 버튼
+const openPopupBtns = document.querySelectorAll('.next-button');
+const popup = document.getElementById('popup');
+const overlay = document.getElementById('overlay');
+
+// 닫기 버튼
+const closeBtn = document.getElementById('close-btn');
+
+// 팝업 닫기
+closeBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+    overlay.style.display = 'none';
+    window.location.href='#'
+});
+ 
