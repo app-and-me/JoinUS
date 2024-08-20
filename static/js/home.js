d0b0e875142e1dfc0dd8f4455f864474de68e002
황유진 <carrot070410@naver.com>
2024-08-20 19:39:53 +0900

    feat : 검색 기능 추가

--- static/js/home.js
+++ static/js/home.js
@@ -64,5 +64,28 @@
         overlay.style.display = 'none';
     });
 
+        // 배경 클릭 시 팝업 닫기
+        overlay.addEventListener('click', () => {
+            popup.style.display = 'none';
+            overlay.style.display = 'none';
+        });
 
- 
+// 검색 기능
+document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('search-bar');
+    const articles = document.querySelectorAll('.article');
+
+    searchInput.addEventListener('input', function() {
+        const searchTerm = searchInput.value.toLowerCase();
+
+        articles.forEach(article => {
+            const clubName = article.querySelector('.club-title').textContent.toLowerCase();
+
+            if (clubName.includes(searchTerm)) {
+                article.style.display = '';
+            } else {
+                article.style.display = 'none';
+            }
+        });
+    });
+});
