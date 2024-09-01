4a5e78c0e186f7bb59ba0bf71d65f4468a2f21c1
Yun juyoung <yunju5090@gmail.com>
2024-09-02 05:05:32 +0900

    modify: add name attribute

--- clubs/urls.py
+++ clubs/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('<int:club_id>', views.get_club_detail, name='get_club_detail'),
     path('<int:club_id>/contents/<int:application_id>', views.update_club_contents, name='update_club_contents'),
     path('/clubs/<int:club_id>/applications', views.get_club_student, name='get_club_student'),
-    path('<int:club_id>/applications/', views.create_club_application),
-    path('<int:club_id>/applications/', views.cancel_club_application),
+    path('<int:club_id>/applications/', views.create_club_application, name='create_club_application'),
+    path('<int:club_id>/applications/', views.cancel_club_application, name='create_club_application'),
 ]
