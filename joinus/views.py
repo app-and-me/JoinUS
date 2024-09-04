7244ced39d61ce344563156e75f66985880e87f8
dayul <dyulchoo@naver.com>
2024-09-04 19:48:35 +0900

    feat : render 구현

--- joinus/views.py
+++ joinus/views.py
@@ -6,3 +6,12 @@ def index(request):
 
 def home(request):
     return render(request, 'home.html')
+
+def teacherHome(request):
+    return render(request, 'teacherHome.html')
+
+def myclub(request):
+    return render(request, 'myclub.html')
+
+def studentPassword(request):
+    return render(request, 'student-password.html')
